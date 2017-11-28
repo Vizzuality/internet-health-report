51a7382518836b10f3a56bf583c3291a018230b8
Tiago Santos <santos.tiago@gmail.com>
2017-11-28 19:56:22 +0100

    Added some styling to post listing

--- www/wp-content/themes/ihr-2018/template-parts/content.php
+++ www/wp-content/themes/ihr-2018/template-parts/content.php
@@ -9,24 +9,21 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <a class="single-post" href="<?php echo get_permalink() ?>">
-	<header class="entry-header">
+<article class="c-single-post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="single-post-header">
         <?php echo the_field('type')?> // <?php echo get_the_category()[0]->cat_name?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-        <div class="post-title">
-            <?php echo the_title(); ?>
+	<div class="single-post-content">
+        <div class="single-post-title">
+            <a href="<?php echo get_permalink() ?>"><?php echo the_title(); ?></a>
         </div>
 
-        <div class="post-image">
+        <div class="single-post-image">
             <img src="<?php the_field( 'image') ?>"
         </div>
 
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-	</footer><!-- .entry-footer -->
-    </a>
+	<footer class="single-post-footer"></footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
