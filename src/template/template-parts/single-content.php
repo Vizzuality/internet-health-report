49155601ed2bb1922bd66a269fa5db6052b87a22
Clara Linos <clara.linos@gmail.com>
2018-01-03 18:32:51 +0100

    social bar

--- src/template/template-parts/single-content.php
+++ src/template/template-parts/single-content.php
@@ -48,7 +48,7 @@
 
       <div class="row">
         <div class="column small-10 small-offset-1">
-          <div class="c-tag" style="background-color:<?php echo the_field('color', 'category_' . get_the_category()[0]->term_id);?>">
+          <div class="c-tag" style="background-color:<?php echo the_field('color', 'category_' . get_the_category()[0]->term_id); ?>">
             <?php echo get_the_category()[0]->cat_name ?>
           </div>
         </div>
@@ -63,6 +63,44 @@
             <?php echo get_post_field('post_content'); ?>
           </div>
         </div><!-- .entry-content -->
+      </div>
+    </div>
+
+    <div class="l-social">
+      <div class="wrap">
+
+        <div class="c-social -left -share">
+          <div class="reactions">
+            <a href="#"><svg class="c-icon -small"><use xlink:href="#icon-facebook"></use></svg></a>
+            <a href="#"><svg class="c-icon -small"><use xlink:href="icon-instagram"></use></svg></a>
+            <a href="#"><svg class="c-icon -small"><use xlink:href="icon-twitter"></use></svg></a>
+          </div>
+          <div class="reactions">
+            <span class="text -btn3">Share this</span>
+          </div>
+        </div>
+
+        <div class="c-social -right -reactions">
+          <div class="reactions">
+            <svg class="c-icon -small"><use xlink:href="#icon-comment_icon"></use></svg>
+            <span class="figure text -btn3">1,986</span>
+            <span class="text -btn3">Comments</span>
+          </div>
+          <div id="reactions-switcher" class="reactions">
+            <div class="reactions-panel">
+              <span class="text -btn3">51 <svg class="c-icon -small"><use xlink:href="#icon-comment_icon"></use></svg></span>
+              <span class="text -btn3">55 <svg class="c-icon -small"><use xlink:href="#icon-comment_icon"></use></svg></span>
+              <span class="text -btn3">140 <svg class="c-icon -small"><use xlink:href="#icon-comment_icon"></use></svg></span>
+              <span class="text -btn3">107 <svg class="c-icon -small"><use xlink:href="#icon-comment_icon"></use></svg></span>
+              <span class="text -btn3">101 <svg class="c-icon -small"><use xlink:href="#icon-comment_icon"></use></svg></span>
+            </div>
+            <svg class="c-icon -small"><use xlink:href="#icon-comment_icon"></use></svg>
+            <span class="figure text -btn3">355</span>
+            <span class="text -btn3">Reactions</span>
+          </div>
+          <button class="btn-add-reaction" style="background-color:<?php echo the_field('color', 'category_' . get_the_category()[0]->term_id); ?>">+</button>
+        </div>
+
       </div>
     </div>
 
