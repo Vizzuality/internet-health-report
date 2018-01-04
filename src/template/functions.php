64766c39f46b58003c0f3f759cf552d12f0cc5cd
Clara Linos <clara.linos@gmail.com>
2018-01-04 12:35:39 +0100

    button styles

--- src/template/functions.php
+++ src/template/functions.php
@@ -126,6 +126,8 @@ add_action( 'widgets_init', 'ihr_2018_widgets_init' );
 function ihr_2018_scripts() {
 	wp_enqueue_style( 'ihr-2018-style', get_stylesheet_uri() );
 
+  wp_enqueue_script( 'ihr-2018-script', get_template_directory_uri() . '/js/script.js', array(), '20151215', true );
+
 	wp_enqueue_script( 'ihr-2018-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'ihr-2018-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
