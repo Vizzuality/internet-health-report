77f8959175ec62fd10525faff0fb66afe8a11413
Tiago Santos <santos.tiago@gmail.com>
2018-01-11 10:26:09 +0000

    Added the functions to filter the results

--- src/template/functions.php
+++ src/template/functions.php
@@ -163,3 +163,63 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+// List of filters allowed
+function register_query_vars( $vars ) {
+	$vars[] = 'type';
+	$vars[] = 'issue';
+    return $vars;
+} 
+add_filter( 'query_vars', 'register_query_vars' );
+
+// array of filters (field key => field name)
+$GLOBALS['my_query_filters'] = array( 
+	'field_2'	=> 'type'
+);
+
+
+// action
+add_action('pre_get_posts', 'my_pre_get_posts', 10, 1);
+
+function my_pre_get_posts( $query ) {
+	// bail early if is in admin
+	if( is_admin() ) return;
+	
+	
+	// bail early if not main query
+	// - allows custom code / plugins to continue working
+	if( !$query->is_main_query() ) return;
+	
+	
+	// get meta query
+	$meta_query = $query->get('meta_query');
+
+	// loop over filters
+	foreach( $GLOBALS['my_query_filters'] as $key => $name ) {
+		// continue if not found in url
+		if( empty($_GET[ $name ]) ) {
+			
+			continue;
+			
+		}
+	
+		// get the value for this filter
+		// eg: http://www.website.com/events?city=melbourne,sydney
+		$value = explode(',', $_GET[ $name ]);
+		
+		// append meta query
+    	$meta_query[] = array(
+            'key'		=> $name,
+            'value'		=> $value,
+            'compare'	=> 'IN',
+        );
+	} 
+
+
+	// update meta query
+	$query->set('meta_query', $meta_query);
+
+	if( !empty($_GET[ 'issue' ]) ) {
+		$query->set('category__in', get_cat_ID($_GET[ 'issue' ]));
+	}
+
+}
