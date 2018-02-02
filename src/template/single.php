9c77d22620510c7d4ba66efad11aabd96c42afd6
Tiago Santos <santos.tiago@gmail.com>
2018-02-02 16:09:11 +0000

    Added related posts manually

--- src/template/single.php
+++ src/template/single.php
@@ -32,6 +32,43 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-	<?php echo do_shortcode('[jetpack-related-posts]'); ?>
+    <div class="relatedposts">
+    <h3>More and more</h3>
+    <?php
+        $orig_post = $post;
+        global $post;
+        $tags = wp_get_post_tags($post->ID);
+ 
+        if ($tags) {
+            $tag_ids = array();
+        foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+            $args=array(
+                'tag__in' => $tag_ids,
+                'post__not_in' => array($post->ID),
+                'posts_per_page'=>5, // Number of related posts to display.
+                'caller_get_posts'=>1
+            );
+ 
+        $my_query = new wp_query( $args );
+ 
+        while( $my_query->have_posts() ) {
+			$my_query->the_post();
+        ?>
+ 
+        <div class="relatedthumb">
+			<p><?php echo the_field('type') ?> // <?php echo get_the_category()[0]->cat_name ?></p>
+            <a rel="external" href="<? the_permalink()?>"><br />
+            <?php the_title(); ?>
+            </a>
+        </div>
+ 
+        <?php }
+        }
+        $post = $orig_post;
+        wp_reset_query();
+        ?>
+    </div>
+
+
 <?php
 get_footer();
