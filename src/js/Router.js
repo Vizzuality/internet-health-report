0f6743bc78c86c13719a5cbb28ce200928c1a90b
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-02-21 19:06:15 +0000

    For the router's URL matching

--- src/js/Router.js
+++ src/js/Router.js
@@ -3,7 +3,9 @@ import HomePage from 'pages/HomePage';
 import ExplorePage from 'pages/ExplorePage';
 import PostPage from 'pages/PostPage';
 
-const baseUrl = `${window.BASE_URL}`;
+const baseUrl = (location.hostname === 'localhost' || location.hostname === '0.0.0.0')
+  ? location.origin
+  : window.BASE_URL;
 
 const routes = [
   {
@@ -27,7 +29,7 @@ export default class Router {
    * @returns {{ match: RegExp, class: typeof HomePage }}
    */
   static getMatchingRoute() {
-    return routes.find(r => r.match.test(location.pathname));
+    return routes.find(r => r.match.test(location.href.replace(baseUrl, '')));
   }
 
   /**
