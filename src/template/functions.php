071fb90e1cd449492a5aa609d539532472387df9
Tiago Santos <santos.tiago@gmail.com>
2018-02-22 16:10:32 +0000

    Removed the request of the assets in the functions using their versions. We now use a plugin to manage the versioning

--- src/template/functions.php
+++ src/template/functions.php
@@ -126,11 +126,11 @@ add_action( 'widgets_init', 'ihr_2018_widgets_init' );
 function ihr_2018_scripts() {
 	wp_enqueue_style( 'ihr-2018-style', get_stylesheet_uri() );
 
-  wp_enqueue_script( 'ihr-2018-script', get_template_directory_uri() . '/js/script.js', array(), '20151215', true );
+  wp_enqueue_script( 'ihr-2018-script', get_template_directory_uri() . '/js/script.js', array(), false, true );
 
-	wp_enqueue_script( 'ihr-2018-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'ihr-2018-navigation', get_template_directory_uri() . '/js/navigation.js', array(), false, true );
 
-  wp_enqueue_script( 'ihr-2018-js-app', get_template_directory_uri() . '/js/app.js', array(), '20151215', true );
+  wp_enqueue_script( 'ihr-2018-js-app', get_template_directory_uri() . '/js/app.js', array(), false, true );
 
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
     wp_enqueue_script( 'comment-reply' );
