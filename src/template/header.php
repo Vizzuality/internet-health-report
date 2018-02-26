d05d444341ed758336a67f223f98f669032ec5c9
Tiago Santos <santos.tiago@gmail.com>
2018-02-26 16:57:51 +0000

    Added google analytics

--- src/template/header.php
+++ src/template/header.php
@@ -13,6 +13,15 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-87658599-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-87658599-2');
+  </script>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
