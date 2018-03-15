6e032e5d7aee18228c97715072249cd7be5305d2
Tiago Santos <santos.tiago@gmail.com>
2018-03-15 15:46:28 +0000

    Added contact-us page template. This needs styling

---
+++ src/template/page-contact-us.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ Template Name: Contact us
+ */
+
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main l-main">
+
+			<?php
+        // Defines active tab
+        $current = get_the_ID();
+
+        // Get parent page info.
+        $parent = $post->post_parent;
+
+        // Determine wheter we are in the section's root
+        if ($parent == 0) {
+          $section_root = true;
+        } else {
+          $section_root = false;
+        };
+
+        // Get the current post info
+        $current_post = get_post($parent);
+        $parent_title = $current_post->post_title;
+        $content = $current_post->post_content;
+
+        // Draw section header
+        ?>
+          <div class="wrap">
+            <div class="l-section-header">
+              <div class="row">
+                <div class="column small-12 medium-8">
+                  <h2><?php echo $parent_title; ?></h2>
+                  <p><?php echo $content; ?></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <?php
+
+          // Determine active section to ask for children
+          if ($section_root) {
+            $active_page = $current;
+          } else {
+            $active_page = $parent;
+          }
+
+          // Determine pages
+          $mypages = get_pages( array( 'child_of' => $active_page, 'sort_column' => 'menu_order', 'sort_order' => 'asc' ) );
+
+          // Draw tabs
+          if(count($mypages) > 0) {
+            // Declare global var with pages to be available in the template
+            set_query_var( 'mypages', $mypages );
+            set_query_var( 'isRoot', $section_root );
+            // Get tabs template
+            get_template_part( 'template-parts/tab' );
+          }
+
+          ?>
+
+          <div id="coral_talk_stream"></div>
+          <script src="http://127.0.0.1:3002/static/embed.js" async onload="
+          Coral.Talk.render(document.getElementById('coral_talk_stream'), {
+              talk: 'http://127.0.0.1:3002'
+          });
+          "></script>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php
+get_footer();
+
+
