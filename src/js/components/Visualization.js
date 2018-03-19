cbc66e70d6ad284985d6aea09530b26c58abcb09
David Inga <david.inga@vizzuality.com>
2018-03-19 13:46:44 +0100

    added new vis app unhappiness

--- src/js/components/Visualization.js
+++ src/js/components/Visualization.js
@@ -5,38 +5,26 @@ import Area from 'components/Visualizations/Area';
 import Circle from 'components/Visualizations/Circle';
 import Wikipedia from 'components/Visualizations/Wikipedia';
 import Map from 'components/Visualizations/Map';
+import AppUnhappiness from 'components/Visualizations/AppUnhappiness';
+
+const vis = {
+  'bar': Bar,
+  'stacked-bar': StackedBar,
+  'line': Line,
+  'area': Area,
+  'circle': Circle,
+  'map': Map,
+
+  // Custom vis
+  'wikipedia': Wikipedia
+  'app-unhappiness': AppUnhappiness
+};
 
 export default class Visualization {
   constructor(container, options) {
-    switch (options.type) {
-      case 'bar':
-        return new Bar(container, options);
-
-      case 'stacked-bar':
-        return new StackedBar(container, options);
-
-      case 'line':
-        return new Line(container, options);
-
-      case 'area':
-        return new Area(container, options);
-
-      case 'circle':
-        return new Circle(container, options);
-
-      case 'wikipedia':
-        return new Wikipedia(container, options);
-
-      case 'map':
-        return new Map(container, options);
-
-      case 'TODO':
-        // eslint-disable-next-line no-param-reassign
-        container.textContent = 'Visualization not implemented yet';
-        return null;
-
-      default:
-        console.error('Unsupported visualization');
-    }
+    const Vis = vis[options.type];
+    if (Vis) return new Vis(container, options);
+    console.error('Unsupported visualization');
+    return null;
   }
 }
---
+++ src/js/components/Visualizations/AppUnhappiness.js
@@ -0,0 +1,161 @@
+import { select } from 'd3-selection';
+import { scaleLinear } from 'd3-scale';
+import { extent, range } from 'd3-array';
+import { axisLeft } from 'd3-axis';
+import { areaRadial, lineRadial, curveLinearClosed } from 'd3-shape';
+import scaleRadial from './scale-radial';
+import AbstractVisualization from './AbstractVisualization';
+
+export default class AppUnhappiness extends AbstractVisualization {
+  constructor(el, config) {
+    super(el, config);
+    this.initialize();
+  }
+
+  initialize() {
+    // fetch and then render
+    this.fetchData()
+      .then(() => this.render());
+  }
+
+  renderChart() {
+    const { el, config } = this;
+
+    // parsing data
+    const data = this.data.map((d) => {
+      return {
+        name: d.label,
+        happy_percentage: Number(d.happy_percentage * 100),
+        avg_minutes_happy: Number(d.avg_minutes_happy * 100),
+        avg_minutes_unhappy: Number(d.avg_minutes_unhappy * 100)
+      };
+    }).sort((a, b) => b.happy_percentage - a.happy_percentage);
+
+    // Creating svg
+    const svg = select(el).append('svg');
+    const margin = { top: 170, left: 50, right: 50, bottom: 50 };
+    const width = el.clientWidth - margin.left - margin.right;
+    const height = el.clientHeight - margin.top - margin.bottom;
+    const radius = height / 2;
+    const dataLength = data.length;
+
+    svg
+      .attr('width', el.clientWidth)
+      .attr('height', el.clientHeight);
+
+    const g = svg.append('g')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    // Scales
+    const rScale = scaleLinear()
+      .domain([0, 100])
+      .range([0, radius])
+
+    const xScale = scaleLinear()
+      .domain([0, dataLength - 1])
+      .range([0, 2 * Math.PI]);
+
+    const yScale = scaleRadial()
+      .domain(extent(data, d => d.happy_percentage)) // 0-100 percentages
+      .range([0, radius]);
+
+    const areaDomain = [].concat(
+      extent(data, d => d.avg_minutes_happy),
+      extent(data, d => d.avg_minutes_unhappy)
+    );
+    const areaScale = scaleRadial()
+      .domain(extent(areaDomain))
+      .range([0, radius]);
+
+    // Area
+    const happyArea = areaRadial()
+      .angle((d, i) => xScale(i))
+      .innerRadius(yScale(0))
+      .outerRadius(d => areaScale(d.avg_minutes_happy));
+
+    const unHappyArea = areaRadial()
+      .angle((d, i) => xScale(i))
+      .innerRadius(yScale(0))
+      .outerRadius(d => areaScale(d.avg_minutes_unhappy));
+
+    g.append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`)
+      .append('path')
+      .attr('class', 'area unhappy')
+      .datum(data)
+      .attr('d', unHappyArea);
+
+    g.append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`)
+      .append('path')
+      .attr('class', 'area happy')
+      .datum(data)
+      .attr('d', happyArea);
+
+    // Line
+    const line = lineRadial()
+      .angle((d, i) => xScale(i))
+      .radius((d, i) => yScale(d.happy_percentage))
+      .curve(curveLinearClosed);
+
+    g.append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`)
+      .append('path')
+      .attr('class', 'line')
+      .datum(data)
+      .attr('d', line);
+
+    // Radial Axis
+    const rAxis = g.append('g')
+        .attr('class', 'r axis')
+        .attr('transform', `translate(${width / 2}, ${height / 2})`)
+      .selectAll('g')
+        .data(rScale.ticks(5))
+      .enter().append('g');
+
+    rAxis.append('circle')
+      .attr('r', rScale);
+
+    rAxis.append('text')
+      .attr('y', d => -rScale(d) - 4)
+      .attr('transform', 'translate(4, 0)')
+      .text(d => d);
+
+    // Outer axis labels
+    const labelRadius = radius + 15;
+    const anglePerItem = (360 / (dataLength));
+    const ga = g.append('g')
+        .attr('class', 'outer-axis-tick')
+        .attr('transform', `translate(${width / 2}, ${height / 2})`)
+      .selectAll('g')
+        .data(range(-270, 90, anglePerItem).reverse())
+      .enter().append('g')
+        .attr('transform', d => `rotate(${-d-anglePerItem})`);
+
+    ga.append('text')
+      .attr('x', labelRadius)
+      .attr('dy', 4)
+      .style('text-anchor', d => ((d < 270 && d > 90) ? 'end' : null))
+      .attr('transform', d => ((d < 270 && d > 90) ? `rotate(180 ${labelRadius}, 0)` : null))
+      .text((d, i) => data[i] ? data[i].name : '');
+  }
+
+  renderLegend() {
+    const { el } = this;
+    const legendElement = document.createElement('div');
+    legendElement.classList.add('app-unhappiness-vis-legend');
+    legendElement.innerHTML = `<ul>
+      <li class="happy-percent-icon">% Happy</li>
+      <li class="happy-icon">Happy Average on Screen (minutes)</li>
+      <li class="unhappy-icon">Unhappy Average on Screen (minutes)</li>
+    </ul>`;
+    el.appendChild(legendElement);
+  }
+
+  render() {
+    this.renderChart();
+    this.renderLegend();
+  }
+}
