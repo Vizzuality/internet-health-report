dacae80b24f60443e2c887e8ef46941665133c0e
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2018-03-20 15:01:05 +0100

    Adds multiple download tooltip

---
+++ src/js/misc/download-report-tooltip.js
@@ -0,0 +1,24 @@
+import tippy from 'tippy.js/dist/tippy.min';
+
+const triggerElements = document.querySelectorAll('.download-tooltip-trigger');
+
+if (triggerElements.length) {
+  triggerElements.forEach((triggerElement) => {
+    const tooltipContent = triggerElement.nextElementSibling;
+
+    tippy(triggerElement, {
+      html: tooltipContent,
+      arrow: true,
+      placement: 'bottom',
+      theme: 'ihr',
+      interactive: true,
+      distance: 15,
+      onShow: () => {
+        tooltipContent.removeAttribute('style');
+      },
+      onHidden: () => {
+        tooltipContent.setAttribute('style', 'display: none');
+      }
+    });
+  });
+}
