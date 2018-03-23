c6f438f81e951bc572ba5afcafaf77613401bb58
Tiago Santos <santos.tiago@gmail.com>
2018-03-23 17:31:54 +0000

    Added a new template for the spotlight post listing

--- src/template/page-trending-topics.php
+++ src/template/page-trending-topics.php
@@ -94,7 +94,7 @@ get_header();
                    * If you want to override this in a child theme, then include a file
                    * called content-___.php (where ___ is the Post Format name) and that will be used instead.
                    */
-                  get_template_part( 'template-parts/content');
+                  get_template_part( 'template-parts/spotlight-content');
                   wp_reset_postdata();
                 endforeach;
               ?>
---
+++ src/template/template-parts/spotlight-content.php
@@ -0,0 +1,43 @@
+<?php
+  $type = 'type-' . get_post_meta($post->ID, 'type', true);
+  $image = get_post_meta($post->ID, 'image', true);
+?>
+
+<div class="column small-12 medium-6 large-4 l-card">
+  <a href="<?php echo get_permalink() ?>" class="c-single-post <?php echo $type ?>" id="post-<?php the_ID(); ?>">
+
+    <span class="color-border" <?php post_class(); ?> style="background-color:<?php the_field('color', 'category_' . get_the_category()[0]->term_id)?>;"></span>
+
+    <?php the_field('color', 'category_' . get_the_category()[0]->term_id)?>
+
+    <div class="single-post-body <?php if($image){echo '-image';}; ?>">
+    	<header class="single-post-header">
+        <p class="text -link -secondary"><?php echo get_the_category()[0]->cat_name?> // <?php echo the_field('type')?></p>
+    	</header><!-- .entry-header -->
+
+      <div class="single-post-title">
+        <p class="text -box1 "><?php echo the_title(); ?></p>
+      </div>
+      <!--
+      <div class="c-comments-band">
+        <span class="comments">
+          <svg class="c-icon -small"><use xlink:href="#icon-comment_icon"></use></svg>
+          <span class="text">381</span>
+        </span>
+        <span class="reactions">
+          <svg class="c-icon -small"><use xlink:href="#icon-comment_icon"></use></svg>
+          <span class="text"><?php 
+          //$reactions = reaction_count(get_the_ID());
+          //echo $reactions['total_reactions'];
+          ?></span>
+        </span>
+      </div>
+      -->
+    </div>
+    <?php
+      if ($image) { ?>
+        <div class='single-post-image' style='background-image:url(<?php the_field('image')?>)'></div>
+      <?php }
+    ?>
+  </a><!-- #post-<?php the_ID(); ?> -->
+</div>
