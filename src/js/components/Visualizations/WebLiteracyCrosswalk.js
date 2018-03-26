2bb5682ca448533cb841313cbdb9208671f3a696
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-03-26 19:24:17 +0100

    Make the visualisation vis_5_6 "responsive"

--- src/js/components/Visualizations/WebLiteracyCrosswalk.js
+++ src/js/components/Visualizations/WebLiteracyCrosswalk.js
@@ -1,7 +1,6 @@
 import { select } from 'd3-selection';
-import tippy from 'tippy.js/dist/tippy.min';
-import AbstractVisualization from './AbstractVisualization';
-import jsonSpec from './WebLiteracyCrosswalk-spec.json';
+import AbstractVisualization from 'components/Visualizations/AbstractVisualization';
+import jsonSpec from 'components/Visualizations/WebLiteracyCrosswalk-spec.json';
 
 let vega = null;
 
@@ -12,6 +11,7 @@ export default class WebLiteracyCrosswalk extends AbstractVisualization {
       .then((lib) => {
         vega = lib;
         this.initialize();
+        this.setListeners();
       });
   }
 
@@ -23,65 +23,14 @@ export default class WebLiteracyCrosswalk extends AbstractVisualization {
 
   getTooltipContent(target) { // eslint-disable-line class-methods-use-this
     const { datum } = select(target).datum();
-    const { dictionary } = this;
 
     return `
-      <p class="title">${datum.subcategory}</p>
-      <p class="note">${datum.category}</p>
-      <p class="number" style="color: ${this.color}">${(datum.value === 1 ? dictionary.yes : dictionary.no)}</p>
-      <p class="note" style="max-width: 200px">${datum.label}</p>
+      <div class="web-literacy-crosswalk">
+        <p class="title">${datum.subcategory}</p>
+        <p class="label">${datum.category}</p>
+        <p>${datum.description}</p>
+      </div
     `;
-  }
-
-  /**
-   * Instantiate the tooltip for the target elements
-   * @param {string} target HTML/CSS selector
-   * @param {string} [trigger='mouseenter focus'] Event(s) to trigger the tooltip
-   * @param {boolean} [sticky=true] Whether the tooltip follows the target
-   * @param {string} additionalClass CSS class to add
-   * @return {any} tippy instance
-   */
-  instantiateTooltip(target, trigger = 'mouseenter focus', sticky = true, additionalClass) {
-    const getTooltipContent = this.getTooltipContent.bind(this);
-    return tippy(target, {
-      animateFill: false,
-      animation: 'shift-toward',
-      inertia: true,
-      theme: 'ihr',
-      html: document.createElement('div'),
-      sticky,
-      arrow: true,
-      interactive: true,
-      trigger,
-      wait(show, e) {
-        // Callback to be executed when the tooltip is inserted in
-        // the dom
-        let callback = () => {};
-
-        // FIXME: We hack tippy here to not have to inject
-        // the result of getTooltipContent in the DOM
-        // Referencing a virtual node in `html` doesn't
-        // work because it can't be re-used
-        const container = document.createElement('div');
-        container.classList.add('c-tooltip', 'js-tooltip');
-        container.innerHTML = getTooltipContent(e.target, (func) => { callback = func; });
-
-        // We add the additional class
-        if (additionalClass) {
-          this.children[0].classList.add(additionalClass);
-        }
-
-        const tooltip = this.querySelector('.tippy-content');
-        tooltip.innerHTML = '';
-        tooltip.appendChild(container);
-
-        // We execute the callback
-        callback(container);
-
-        show();
-      },
-      onHide: this.onHideTooltip.bind(this)
-    });
   }
 
   vegaSpec() {
@@ -100,26 +49,39 @@ export default class WebLiteracyCrosswalk extends AbstractVisualization {
     return spec;
   }
 
-  renderChart() {
-    const { el } = this;
+  render() {
+    super.render();
+    if (!this.data) return;
 
-    // Vega
+    this.el.classList.add('v-web-literacy-crosswalk');
+
     const runtime = vega.parse(this.vegaSpec());
 
     new vega.View(runtime) // eslint-disable-line no-new
       .logLevel(vega.Warn)
-      .initialize(el)
+      .initialize(this.el)
       .renderer('svg')
-      .run();
+      .runAsync()
+      .then(() => {
+        this.svg = select(this.el.querySelector('svg'));
 
-    setTimeout(() => this.instantiateTooltip(document.querySelectorAll('.mark-arc path')), 10); // :hankey:
-  }
+        this.svg
+          .attr('width', this.width * this.scale)
+          .attr('height', this.height * this.scale)
+          .attr('viewBox', `0 0 ${this.width} ${this.height}`)
+          .attr('role', 'img')
+          .attr('aria-labelledby', `title_${this.id} desc_${this.id}`);
 
-  render() {
-    if (!this.data) {
-      this.el.innerHTML = 'Unable to load the data!';
-    }
+        this.svg.append('title')
+          .attr('id', `title_${this.id}`)
+          .text(this.title);
 
-    this.renderChart();
+        this.svg.append('desc')
+          .attr('id', `desc_${this.id}`)
+          .text(this.description);
+
+        this.el.setAttribute('title', this.title);
+        this.instantiateTooltip('.mark-arc path');
+      });
   }
 }
