b36dcc57c3ee5f44dd22fafa00bde5979de41160
Tiago Santos <santos.tiago@gmail.com>
2018-04-06 09:41:50 +0100

    Changed the homepage to a template

--- src/template/page-home.php
+++ src/template/page-home.php
@@ -1,16 +1,6 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package ihr-2018
- *
+/*
+ Template Name: Home Page
  */
 
 get_header(); ?>
