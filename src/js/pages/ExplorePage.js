Vizzuality/internet-health-report

commit 1f5ca0afe4ffb0d2fa24405d1d131c9d956039a3

Enhance the default select inputs

src/js/pages/ExplorePage.js
@@ -1,0 +1,17 @@
+import EnhancedSelect from 'components/EnhancedSelect';
+
+export default class ExplorePage {
+  constructor(_, { baseUrl }) {
+    this.baseUrl = baseUrl;
+    this.enhanceFilters();
+  }
+
+  enhanceFilters() { // eslint-disable-line class-methods-use-this
+    const filters = document.querySelectorAll('select.js-filter');
+    const form = filters.length
+      ? filters[0].closest('form')
+      : null;
+
+    filters.forEach(filter => new EnhancedSelect(filter, () => form.submit()));
+  }
+}

src/js/Router.js
@@ -9,6 +9,10 @@ const routes = [
     match: /^\/category\/[A-z0-9-]+\/?(\?lang=[a-z]{2})?$/,
     page: 'IssuePage'
   },
+  {
+    match: /^\/\?(.*)?s=.*(.*)?$/,
+    page: 'ExplorePage'
+  },
   // Should be last
   {
     match: /^\/[A-z0-9-]+\/?(\?lang=[a-z]{2})?$/,

src/js/components/EnhancedSelect.js
@@ -1,0 +1,82 @@
+import tippy from 'tippy.js/dist/tippy.min';
+
+export default class EnhancedSelect {
+  constructor(el, onChange = () => {}) {
+    this.el = el;
+    this.onChange = onChange;
+    this.initialize();
+  }
+
+  initialize() {
+    const div = document.createElement('div');
+    div.classList.add('c-enhanced-select');
+    div.setAttribute('aria-label', this.el.getAttribute('aria-label'));
+
+    const label = document.createElement('button');
+    label.setAttribute('type', 'button');
+    label.classList.add('label');
+    label.textContent = this.el.selectedOptions[0].textContent;
+
+    div.appendChild(label);
+
+    const tooltipContent = document.createElement('div');
+    tooltipContent.classList.add('c-enhanced-select');
+
+    const options = document.createElement('ul');
+    options.classList.add('options');
+    options.innerHTML = Array.prototype.slice.call(this.el.options).reduce((res, o) => {
+      return `
+        ${res}
+        <li class="${this.el.selectedOptions[0] === o ? '-selected' : ''}">
+          <button type="button" data-value="${o.value}">${o.textContent}</button>
+        </li>
+      `;
+    }, '');
+
+    options.addEventListener('click', (e) => {
+      if (!e.target.matches('.options button')) return;
+
+      label.textContent = e.target.textContent;
+      this.closeTooltip();
+
+      const option = Array.prototype.slice.call(this.el.options).find(o => `${o.value}` === e.target.getAttribute('data-value'));
+      option.selected = true;
+
+      this.onChange(option);
+    });
+
+    tooltipContent.appendChild(options);
+
+    // We add the tooltip
+    tippy(label, {
+      animateFill: false,
+      animation: 'shift-toward',
+      inertia: true,
+      theme: 'ihr',
+      html: tooltipContent,
+      arrow: true,
+      interactive: true,
+      trigger: 'click'
+    });
+
+    // We create a new parent element to the filter
+    const parent = this.el.parentElement;
+    parent.insertBefore(div, this.el);
+    div.appendChild(this.el);
+  }
+
+  /**
+   * Close the tooltip
+   */
+  closeTooltip() { // eslint-disable-line class-methods-use-this
+    // We close the tooltip, if opened
+    document.querySelectorAll('.tippy-popper')
+      .forEach((popper) => {
+        const instance = popper._tippy; // eslint-disable-line no-underscore-dangle
+        if (instance.state.visible) {
+          instance.popperInstance.disableEventListeners();
+          instance.hide();
+        }
+      });
+  }
+}

src/js/index.js
@@ -2,7 +2,6 @@ import 'misc/navigation';
 import 'misc/skip-link-focus-fix';
 import 'misc/analytics';
 import 'misc/language-switcher';
-import 'misc/explore-filters';
 import 'misc/download-report-tooltip';
 import 'misc/social';
 import 'misc/reactions';

src/js/misc/explore-filters.js
@@ -1,14 +1,0 @@
-
-const filters = document.querySelectorAll('select.js-filter');
-const form = document.getElementById('archive-filters');
-const handleChangeFilter = (e) => {
-  const filterBy = e.currentTarget.name;
-  window.logEvent('Explore', `Filter by ${filterBy}`, e.currentTarget.value);
-  form.submit();
-};
-
-if (filters.length && form) {
-  filters.forEach((filter) => {
-    filter.addEventListener('change', handleChangeFilter);
-  });
-}

src/js/misc/language-switcher.js
@@ -1,11 +1,11 @@
+import EnhancedSelect from 'components/EnhancedSelect';
+
 // This code is for the language selector located
 // in the header and footer
 const els = document.querySelectorAll('.js-language-switcher');
 if (els.length) {
-  els.forEach((el) => {
-    el.addEventListener('change', ({ target }) => {
-      window.logEvent('Change Language', 'Clicks to change language', target.selectedOptions[0].getAttribute('data-lang'));
-      window.location = target.value;
-    });
-  });
+  els.forEach(el => new EnhancedSelect(el.children[0], (option) => {
+    window.logEvent('Change Language', 'Clicks to change language', option.getAttribute('data-lang'));
+    window.location = option.value;
+  }));
 }

src/scss/components/c-enhanced-select.scss
@@ -1,0 +1,42 @@
+.c-enhanced-select {
+  font-size: $font-s;
+
+  select {
+    display: none !important;
+  }
+
+  .label {
+    padding: 6px 25px 6px 6px;
+    font-size: inherit;
+    background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMiAzMiI+PHBhdGggZD0iTTE2LjkxNCAyNS4zNzFMMi4wNTcgMTEuNjU3bDQuNTcxLTUuMDI5IDEwLjI4NiA5LjM3MUwyNy4yIDYuNjI4bDQuOCA1LjAyOXoiIGZpbGw9IiMwMDAiLz48L3N2Zz4K');
+    background-repeat: no-repeat;
+    background-size: 11px auto;
+    background-position: right center;
+  }
+
+  .options {
+
+    li {
+      cursor: pointer;
+
+      &:hover {
+        background: $black;
+
+        button {
+          color: $white;
+        }
+      }
+
+      &.-selected {
+        font-weight: 600;
+      }
+
+      button {
+        width: 100%;
+        height: 100%;
+        text-align: left;
+        font-size: inherit;
+      }
+    }
+  }
+}

src/scss/components/c-explore-filters.scss
@@ -7,6 +7,13 @@
     display: flex;
     flex-wrap: wrap;
 
+    // Enhanced selects
+    > div {
+      &:not(:last-of-type) {
+        margin-right: 20px;
+      }
+    }
+
     select {
       border: none;
       background: transparent;

commit 4cc05a3285de3322e7d21ed7c91c3228e37e0df1

Fix a regression from 1f5ca0a where some analytics disappeared
When the user would select an issue or a type of post on the Explore page, the events weren't sent.

src/js/pages/ExplorePage.js
@@ -12,6 +12,11 @@ export default class ExplorePage {
       ? filters[0].closest('form')
       : null;
 
-    filters.forEach(filter => new EnhancedSelect(filter, () => form.submit()));
+    filters.forEach(filter => new EnhancedSelect(filter, (option) => {
+      const filterName = option.parentElement.getAttribute('name');
+      const value = option.textContent;
+      window.logEvent('Explore', `Filter by ${filterName}`, value);
+      setTimeout(() => form.submit(), 200);
+    }));
   }
 }