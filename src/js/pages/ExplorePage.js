72e06071d004ebdacb19de30f6b7aec6df0b7829
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-04-23 16:02:56 +0100

    Fix a regression from aeea062 where some analytics were removed
    The event attached to the search input of the Explore page was removed.

--- src/js/pages/ExplorePage.js
+++ src/js/pages/ExplorePage.js
@@ -4,6 +4,7 @@ export default class ExplorePage {
   constructor(_, { baseUrl }) {
     this.baseUrl = baseUrl;
     this.enhanceFilters();
+    this.trackKeywordSearch();
   }
 
   enhanceFilters() { // eslint-disable-line class-methods-use-this
@@ -16,7 +17,23 @@ export default class ExplorePage {
       const filterName = option.parentElement.getAttribute('name');
       const value = option.textContent;
       window.logEvent('Explore', `Filter by ${filterName}`, value);
+      // NOTE: form.submit() doesn't fire a submit event so
+      // trackKeywordSearch won't fire a second event
       setTimeout(() => form.submit(), 200);
     }));
+  }
+
+  trackKeywordSearch() { // eslint-disable-line class-methods-use-this
+    const input = document.querySelector('input.js-filter');
+    const form = input.closest('form');
+
+    if (form) {
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        window.logEvent('Explore', 'Search text', input.value);
+
+        setTimeout(() => form.submit(), 200);
+      });
+    }
   }
 }

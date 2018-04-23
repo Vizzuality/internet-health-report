Vizzuality/internet-health-report

commit aeea062cdc55f839c77ecf07034aa85c2bc66717

Improve the Explore's search bar

src/js/pages/ExplorePage.js
@@ -1,48 +1,0 @@
-export default class ExplorePage {
-  constructor(_, { baseUrl }) {
-    this.baseUrl = baseUrl;
-    this.setListeners();
-  }
-
-  setListeners() {
-    const searchButton = document.querySelector('.js-search-button');
-    if (searchButton) {
-      searchButton.addEventListener('click', this.onClickSearch.bind(this));
-    }
-  }
-
-  /**
-   * Event handler executed when the user clicks the
-   * search button
-   */
-  onClickSearch() { // eslint-disable-line class-methods-use-this
-    const filters = Array.prototype.slice.call(document.querySelectorAll('.js-filter'));
-
-    filters.forEach((filter) => {
-      const filterName = filter.getAttribute('data-filter');
-      if (filter.value) {
-        switch (filterName) {
-          case 'issue':
-            window.logEvent('Explore', 'Filter by issue', filter.value);
-            return;
-
-          case 'type':
-            window.logEvent('Explore', 'Filter by type', filter.value);
-            return;
-
-          case 's':
-            window.logEvent('Explore', 'Search text', filter.value);
-            break;
-
-          default:
-        }
-      }
-    });
-
-    const query = filters
-      .map(filter => `${filter.getAttribute('data-filter')}=${filter.value}`)
-      .reduce((res, s, i) => `${res}${i === 0 ? '?' : '&'}${s}`, '');
-
-    window.location = `${this.baseUrl}/${query}`;
-  }
-}

src/js/Router.js
@@ -6,10 +6,6 @@ const routes = [
     page: 'HomePage'
   },
   {
-    match: /^\/search-result\/?$/,
-    page: 'ExplorePage'
-  },
-  {
     match: /^\/category\/[A-z0-9-]+\/?$/,
     page: 'IssuePage'
   },

src/js/pages/HomePage.js
@@ -1,48 +1,5 @@
 export default class HomePage {
   constructor(_, { baseUrl }) {
     this.baseUrl = baseUrl;
-    this.setListeners();
-  }
-
-  setListeners() {
-    const searchButton = document.querySelector('.js-search-button');
-    if (searchButton) {
-      searchButton.addEventListener('click', this.onClickSearch.bind(this));
-    }
-  }
-
-  /**
-   * Event handler executed when the user clicks the
-   * search button
-   */
-  onClickSearch() { // eslint-disable-line class-methods-use-this
-    const filters = Array.prototype.slice.call(document.querySelectorAll('.js-filter'));
-
-    filters.forEach((filter) => {
-      const filterName = filter.getAttribute('data-filter');
-      if (filter.value) {
-        switch (filterName) {
-          case 'issue':
-            window.logEvent('Explore', 'Filter by issue', filter.value);
-            return;
-
-          case 'type':
-            window.logEvent('Explore', 'Filter by type', filter.value);
-            return;
-
-          case 's':
-            window.logEvent('Explore', 'Search text', filter.value);
-            break;
-
-          default:
-        }
-      }
-    });
-
-    const query = filters
-      .map(filter => `${filter.getAttribute('data-filter')}=${filter.value}`)
-      .reduce((res, s, i) => `${res}${i === 0 ? '?' : '&'}${s}`, '');
-
-    window.location = `${this.baseUrl}/${query}`;
   }
 }

src/scss/components/c-explore-filters.scss
@@ -1,0 +1,61 @@
+.c-explore-filters {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  .selectors {
+    display: flex;
+
+    select {
+      border: none;
+      background: transparent;
+      -webkit-appearance: none;
+        -moz-appearance: none;
+              appearance: none;
+      border-radius: 0;
+      display: block;
+      font-size: 20px;
+      font-family: "ZillaSlab", serif;
+      padding: 6px 25px 6px 6px;
+      background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMiAzMiI+PHBhdGggZD0iTTE2LjkxNCAyNS4zNzFMMi4wNTcgMTEuNjU3bDQuNTcxLTUuMDI5IDEwLjI4NiA5LjM3MUwyNy4yIDYuNjI4bDQuOCA1LjAyOXoiIGZpbGw9IiMwMDAiLz48L3N2Zz4K');
+      background-repeat: no-repeat;
+      background-size: 11px auto;
+      background-position: right center;
+
+      &:not(:last-of-type) {
+        margin-right: 20px;
+      }
+    }
+
+    select:-ms-expand {
+      display: none;
+    }
+  }
+
+  .inputs {
+    display: flex;
+    justify-content: flex-end;
+
+    input {
+      flex-grow: 1;
+      border: 0;
+      border-bottom: 3px solid #000;
+      border-radius: 0;
+      font-family: "ZillaSlab", serif;
+      font-size: 20px;
+      font-weight: 300;
+
+      @media screen and (min-width: 40em) {
+        max-width: 300px;
+      }
+    }
+
+    button {
+      flex-shrink: 0;
+      display: inline-block;
+      margin-left: 20px;
+      padding-left: 20px;
+      border-left: 1px solid #000;
+    }
+  }
+}

src/scss/style.scss
@@ -757,40 +757,6 @@ a:active {
 }
 
 /*--------------------------------------------------------------
-# Explore Search
---------------------------------------------------------------*/
-
-.selector-search {
-  position: relative;
-  display: block;
-
-  select {
-    border: none;
-    outline: none;
-    background: transparent;
-    -webkit-appearance: none;
-       -moz-appearance: none;
-            appearance: none;
-    border-radius: 0;
-    margin: 1px;
-    display: block;
-    width: 100%;
-    padding: 12px 55px 15px 15px;
-    font-size: 20px;
-    font-family: "ZillaSlab", serif;
-    padding-right: 25px;
-    background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzMiAzMiI+PHBhdGggZD0iTTE2LjkxNCAyNS4zNzFMMi4wNTcgMTEuNjU3bDQuNTcxLTUuMDI5IDEwLjI4NiA5LjM3MUwyNy4yIDYuNjI4bDQuOCA1LjAyOXoiIGZpbGw9IiMwMDAiLz48L3N2Zz4K');
-    background-repeat: no-repeat;
-    background-size: 11px auto;
-    background-position: right center;
-  }
-
-  select:-ms-expand {
-    display: none;
-  }
-}
-
-/*--------------------------------------------------------------
 # Introduction credits
 --------------------------------------------------------------*/
 
@@ -1561,7 +1527,7 @@ object {
   }
 }
 
-@media screen and (min-width: 40em) and (min-width: 40em) {
+@media screen and (min-width: 40em) {
   .medium-expand {
     -webkit-flex: 1 1 0px;
     -ms-flex: 1 1 0px;
@@ -1899,4 +1865,5 @@ object {
 @import "components/c-newsletter";
 @import "components/c-tooltip";
 @import "components/c-issue-card";
+@import "components/c-explore-filters";
 @import "visualizations/*";

src/template/functions.php
@@ -329,7 +329,7 @@ function tuts_mce_before_init( $settings ) {
 function languages_list_footer() {
   $languages = icl_get_languages('skip_missing=0&orderby=code');
   if(!empty($languages)) {
-    echo '<div class="c-language-switcher js-language-switcher"><select aria-label="Language selector">';
+    echo '<div class="c-language-switcher js-language-switcher"><select aria-label="' . translate( 'Language selector', 'ihr-2018' ) . '">';
     foreach($languages as $l) {
       echo '<option' . ($l['active'] ? ' selected' : '') . ' value="' . $l['url'] . '" data-lang="' . $l['native_name'] . '">';
       echo icl_disp_language($l['native_name'], $l['translated_name']);
@@ -342,13 +342,13 @@ function languages_list_footer() {
 /**
  * Gets the number of comments for a post
  * Queries the Talk API to get the results
- * 
+ *
  * @param integer $post_id The id of the post to count the comments
  * @return integer The number of comments of a post
  */
   function comments_count($post_id) {
     $permalink = get_post_permalink($post_id);
-    $response = wp_remote_post('https://talk.mofoprod.net/api/v1/graph/ql', 
+    $response = wp_remote_post('https://talk.mofoprod.net/api/v1/graph/ql',
           array(
             'method' => 'POST',
             'httpversion' => '1.0',
@@ -356,7 +356,7 @@ function languages_list_footer() {
             'headers' => array("Content-type" => "application/json"),
             'body' => '{"query":"{\n  asset(url: \\"'. $permalink .'\\") {\n    id\n    title\n    commentCount\n  }\n}","variables":null,"operationName":null}',
             'cookies' => array()
-            )); 
+            ));
 
     try {
       preg_match('~commentCount\":(\d+)~', $stuff['body'], $comment_num);
@@ -370,10 +370,10 @@ function languages_list_footer() {
 
 /**
  * Gets the information about a post's reactions
- * 
+ *
  * @param integer $post_id The id of the post to get the reactions counts from
  * @return array [Reaction Count, Most Reacted, Second Most reacted]
- * 
+ *
  */
 
  function reaction_count($post_id) {
@@ -391,9 +391,9 @@ function languages_list_footer() {
 
     $reaction_0_name =  $reaction_names[$reaction_0];
     $reaction_1_name =  $reaction_names[$reaction_1];
-    
+
     $return_array = array('total_reactions' => $reaction_count[0]->total_reactions, 'reaction_0' => $reaction_0_name, 'reaction_1' => $reaction_1_name);
-    
+
     return $return_array;
  }
 

src/template/template-parts/search-bar.php
@@ -1,15 +1,15 @@
 <?php // Search filters ?>
-	<div id="archive-filters" class="wrap">
-    <div class="row">
-    	<div class="column small-6 medium-3 large-2">
-        <div class="selector-search">
-          <select class="js-filter" data-filter="issue">
+<div class="row">
+  <div class="column small-12">
+    <form id="archive-filters" class="wrap c-explore-filters" method="get">
+      <div class="selectors">
+        <select class="js-filter" name="issue" aria-label="<?php _e( 'Filter by issue', 'ihr-2018' ); ?>">
           <?php
             $selected_category = $_GET['issue'];
             if ($selected_category === "" || $selected_category === NULL) {
-              echo "<option value=\"\" selected>All Issues</option>";
+              echo "<option value=\"\" selected>". translate( 'All Issues', 'ihr-2018' ) ."</option>";
             } else {
-              echo "<option value=\"\">All Issues</option>";
+              echo "<option value=\"\">". translate( 'All Issues', 'ihr-2018' ) ."</option>";
             }
 
             foreach(get_categories() as $category)
@@ -19,57 +19,51 @@
               echo "<option value=\"$category->name\">$category->name</option>";
             }
           ?>
-          </select>
-        </div>
-      </div>
+        </select>
 
-      <?php foreach( $GLOBALS['my_query_filters'] as $key => $name ):
-        // get the field's settings without attempting to load a value
-        $field = get_field_object($name);
+        <?php foreach( $GLOBALS['my_query_filters'] as $key => $name ):
+          // get the field's settings without attempting to load a value
+          $field = get_field_object($name);
 
-        $current_value = NULL;
-        // set value if available
-        if( isset($_GET[ $name ]) ) {
+          $current_value = NULL;
+          // set value if available
+          if( isset($_GET[ $name ]) ) {
 
-          //$field['value'] = explode(',', $_GET[ $name ]);
-          $current_value = $_GET[ $name];
+            //$field['value'] = explode(',', $_GET[ $name ]);
+            $current_value = $_GET[ $name];
 
-        }
+          }
 
-        // create filter
+          // create filter
         ?>
-        <div class="column small-6 medium-3 large-2">
-          <div class="selector-search">
-          <select class="js-filter" data-filter="<?php echo $name; ?>">
-          <?php
-            if ($current_value === "" || $current_value === NULL) {
-              echo "<option value=\"\" selected>All {$name}s</option>";
-            } else {
-              echo "<option value=\"\">All {$name}s</option>";
-            }
-            foreach($field['choices'] as $choice) {
-              if (strcasecmp($current_value , $choice) === 0) {
-                echo "<option value=$choice selected>$choice</option>";
+          <select class="js-filter" name="type" aria-label="<?php _e( 'Filter by type', 'ihr-2018' ); ?>">
+            <?php
+              if ($current_value === "" || $current_value === NULL) {
+                echo "<option value=\"\" selected>". translate( 'All Types', 'ihr-2018' ) ."</option>";
               } else {
-                echo "<option value=\"$choice\">$choice</option>";
+                echo "<option value=\"\">". translate( 'All Types', 'ihr-2018' ) ."</option>";
               }
-            }
-          ?>
+              foreach($field['choices'] as $choice) {
+                if (strcasecmp($current_value , $choice) === 0) {
+                  echo "<option value=$choice selected>$choice</option>";
+                } else {
+                  echo "<option value=\"$choice\">$choice</option>";
+                }
+              }
+            ?>
           </select>
-        </div>
-  </div>
-
-	<?php
-		endforeach;
-	?>
-		<div class="column small-12 medium-6 large-8" style="text-align:right;">
-
-      <div class="search-bar">
-        <input type="text" class="js-filter" data-filter="s" value="<?php echo $_GET["s"] ?>">
-        <button id="search-button" type="button" class="js-search-button">Search</button>
+        <?php
+          endforeach;
+        ?>
       </div>
-		</div>
-	</div>
-	</div>
+      <div class="inputs">
+        <input type="text" class="js-filter" name="s" value="<?php echo $_GET["s"] ?>" placeholder="<?php _e( 'Keyword search', 'ihr-2018' ); ?>" aria-label="<?php _e( 'Keyword search', 'ihr-2018' ); ?>">
+        <button id="search-button" type="submit" class="js-search-button" aria-label="<?php _e('Search', 'ihr-2018' ); ?>">
+          <svg class="c-icon -small"><use xlink:href="#icon-search"></use></svg>
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
 
 

commit 72e06071d004ebdacb19de30f6b7aec6df0b7829

Fix a regression from aeea062 where some analytics were removed
The event attached to the search input of the Explore page was removed.

src/js/pages/ExplorePage.js
@@ -4,6 +4,7 @@ export default class ExplorePage {
   constructor(_, { baseUrl }) {
     this.baseUrl = baseUrl;
     this.enhanceFilters();
+    this.trackKeywordSearch();
   }
 
   enhanceFilters() { // eslint-disable-line class-methods-use-this
@@ -16,7 +17,23 @@ export default class ExplorePage {
       const filterName = option.parentElement.getAttribute('name');
       const value = option.textContent;
       window.logEvent('Explore', `Filter by ${filterName}`, value);
+      // NOTE: form.submit() doesn't fire a submit event so
+      // trackKeywordSearch won't fire a second event
       setTimeout(() => form.submit(), 200);
     }));
+  }
+
+  trackKeywordSearch() { // eslint-disable-line class-methods-use-this
+    const input = document.querySelector('input.js-filter');
+    const form = input.closest('form');
+
+    if (form) {
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        window.logEvent('Explore', 'Search text', input.value);
+
+        setTimeout(() => form.submit(), 200);
+      });
+    }
   }
 }